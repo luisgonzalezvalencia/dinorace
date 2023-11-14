4747d5618c05dbb406c4db5fb7b7eff99d2203c2
Luis Gonzalez <luisarturogonzalezvalencia@gmail.com>
2023-11-13 23:15:57 -0300

    Prefabs y SyncVars updated

--- Assets/SyncVars.cs
+++ Assets/SyncVars.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using TMPro;
 
 public class SyncVars : NetworkBehaviour
 {
-    [SyncVar]
+
+    [SerializeField] private TextMeshPro nameText;
+    [SerializeField] private Renderer renderMaterial;
+
+    [SyncVar(hook = nameof(WhenPlayerNameChange))]
     [SerializeField] private string PlayerName;
 
     [Server]
     public void SetPlayerName(string name)
     {
         this.PlayerName = name;
+    }
+
+    private void WhenPlayerNameChange(string oldName, string newName){
+       nameText.text = newName;
     }
 }
